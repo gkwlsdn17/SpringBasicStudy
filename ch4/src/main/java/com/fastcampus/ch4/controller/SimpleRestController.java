0ca5e9beff911316cd6dddab513a917b16659efa
gkwlsdn17 <gkwlsdn17@gmail.com>
2022-03-30 17:04:55 +0900

    Ajax, REST API

---
+++ ch4/src/main/java/com/fastcampus/ch4/controller/SimpleRestController.java
@@ -0,0 +1,26 @@
+package com.fastcampus.ch4.controller;
+import com.fastcampus.ch4.domain.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+//사용하려면 Maven Dependency에서 Jackson Databind 추가 필요
+
+//@Controller
+@RestController
+public class SimpleRestController {
+//    @GetMapping("/ajax")
+//    public String ajax() {
+//        return "ajax";
+//    }
+
+    @PostMapping("/send")
+//    @ResponseBody
+    public Person test(@RequestBody Person p) {
+        System.out.println("p = " + p);
+        p.setName("ABC");
+        p.setAge(p.getAge() + 10);
+
+        return p;
+    }
+    
+}
